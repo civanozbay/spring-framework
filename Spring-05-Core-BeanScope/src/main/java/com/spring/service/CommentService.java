78730f8052ac1f25d7f0da23ddf97d352dc8d3a6
civanozbay <civan417@hotmail.com>
2022-08-27 23:32:16 +0200

    Qualifier

--- Spring-05-Core-BeanScope/src/main/java/com/spring/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/spring/service/CommentService.java
@@ -4,9 +4,13 @@ import com.spring.model.Comment;
 import com.spring.proxy.CommentNotificationProxy;
 import com.spring.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository;
