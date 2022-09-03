34aa731d208be9b699c7ba97ece49b3aec937c05
civanozbay <civan417@hotmail.com>
2022-09-03 21:35:55 +0200

    skeleton of html

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // anootate the class with the @Controller stereotype annotation
+public class HomeController {
+    @RequestMapping("/home") //use @RequestMapping annotation to associate the action with an HTTP request path
+    public String home(){
+        return "home.html"; // return the HTML document name that contains the details we want the browser to display
+    }
+
+    @RequestMapping("/welcome")
+    public String home2(){
+        return "welcome.html";
+    }
+    @RequestMapping
+    public String home3(){
+        return "welcome.html";
+    }
+}
---
+++ Spring-08-Spring-MVC-Core/src/main/java/com/spring/Spring08SpringMvcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringMvcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringMvcCoreApplication.class, args);
+    }
+
+}
