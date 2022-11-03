3021c417f6449dd7531981140ed1c5230af0e6c2
civanozbay <civan417@hotmail.com>
2022-11-03 21:41:07 +0100

    ciname added

--- Spring-13-ORM-CinemaLab/src/main/java/com/cinema/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cinema/repository/CinemaRepository.java
@@ -2,7 +2,10 @@ package com.cinema.repository;
 
 import com.cinema.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -13,25 +16,31 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
 
     //Write a derived query to get cinema with a specific name
-
+    List<Cinema> findByName(String name);
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-
+    List<Cinema> findTop3ByNameContains(String pattern);
     //Write a derived query to list all cinemas in a specific country
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
-
+    List<Cinema> findByNameOrSponsoredName(String name,String sponsorName);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
-
+    @Query("SELECT c FROM Cinema c where c.id= ?1")
+    Cinema getCinemaById(int id);
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all cinemas by location country
-
+//    @Query(value = "select * from cinema where l")
+//    List<Cinema> getCinemasByLocation();
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
-
+//    @Query("select name,sponsored_name from cinemas where contains(name,%?1,)")
+//    List<Cinema> getCinemasContainsNameOrSponsoredName();
     //Write a native query to sort all cinemas by name
-
+    @Query(value = "SELECT * from cinema where name= ?1",nativeQuery = true)
+    List<Cinema> getAllCinemasByName(String name);
     //Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT distinct * from where sponsored_name=?1",nativeQuery = true)
+    List<Cinema> getCinemaByDistinctWithSponsoredName(String name);
 
 }
