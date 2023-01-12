1f0160ade60db813c2ec54ff65fc3dfc4e619408
civanozbay <civan417@hotmail.com>
2023-01-12 23:36:20 +0100

    docker

---
+++ Spring-24-Docker/src/test/java/com/spring/spring24docker/Spring24DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring24docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring24DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
